9b6160394748666493185aebbdb0c2701db18531
kashifali88 <kashifzan28406@gmail.com>
2026-03-07 02:52:12 -0800

    nuyb

--- backend/index.js
+++ backend/index.js
@@ -26,8 +26,7 @@ const connect = async () => {
 
 //middlewares
 server.use(cors({
-  origin: "https://bluewood-ecommerce-app.onrender.com"
-  ,  
+  origin: "https://bluewood-ecommerce-app.onrender.com",
   credentials: true, 
 }))
 server.use(express.json())
